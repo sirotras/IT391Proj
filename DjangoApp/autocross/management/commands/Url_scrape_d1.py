5f23d13a797c8787ad0d6ebd3a147b7291f8177c
Jcausa99 <--global>
2022-10-19 17:14:23 -0500

    FB_workinprogress

--- DjangoApp/autocross/management/commands/Url_scrape_d1.py
+++ DjangoApp/autocross/management/commands/Url_scrape_d1.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import mechanicalsoup
 import sys
+import datefinder
 
 sys.tracebacklimit = 0
 
@@ -9,28 +10,44 @@ browser = mechanicalsoup.StatefulBrowser()
 URL = "https://mbasic.facebook.com/ccsportscarclub?v=events&is_past=1&refid=17"
 browser.open(URL)
 
-page = browser.get_current_page().find_all("div", class_="be")
+
+
+page = browser.get_current_page().find_all('span', class_='bi')
 
 for x in page:
-    print(x.text)
+  print(x.text)
+  dates = datefinder.find_dates(x.text)
+  location_list = {'Rantoul':'Rantoul, IL', 'Urbana':'Urbana, IL','Mahomet':'Mahomet, IL'}
+  location = ""
+  for key,value in location_list.items():
+    if x.text.find(key) != -1:
+      location = value
+      print(location)
+  
+  
 
-browser.get_url()
-print(browser.get_url())
-print("-------------------------------------")
-#print(page.get_text())
-print("--------------------------------------")
+  for match in dates:
+    print(match)
+    
+  print("---------------------------")
+ # f = open("FBoutput.txt","a")
+  #f.write(x.text + '\n')
+  #f.close()
+
+#browser.get_url()
+#print(browser.get_url())
+#print("-------------------------------------")
 
 
-
-#while True:
-  #  if browser.follow_link(text = "See More Events"):
-   #     browser.get_url()
-    #    page = browser.get_current_page().find("div", class_="be")
-     #   print(browser.get_url())
-      #  print(page.get_text())
-       # print("---------------------------------------")
-        
-    #else:
-     #   break
+""" 
+while True:
+    if browser.follow_link(text = "See More Events"):
+        browser.get_url()
+        page = browser.get_current_page().find_all('span', class_='bi')
+        for x in page:
+          f = open("FBoutput.txt", "a")
+          f.write(x.text + '\n')
+          f.close()
+ """
 
 
